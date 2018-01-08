3f7887e95eb86df44b831ebb705fded87687da71
Prakash Raman <prakash.raman.ka@gmail.com>
2018-01-08 14:12:32 +0530

    Initial Commit

---
+++ PineSwiftStarter/PineSwiftStarter.h
@@ -0,0 +1,19 @@
+//
+//  PineSwiftStarter.h
+//  PineSwiftStarter
+//
+//  Created by Prakash Raman on 08/01/18.
+//  Copyright © 2018 Prakash Raman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PineSwiftStarter.
+FOUNDATION_EXPORT double PineSwiftStarterVersionNumber;
+
+//! Project version string for PineSwiftStarter.
+FOUNDATION_EXPORT const unsigned char PineSwiftStarterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PineSwiftStarter/PublicHeader.h>
+
+
